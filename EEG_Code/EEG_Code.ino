9cede30e0c1d4df27d18bf722418c5cc48ca2a87
James Grissom <72823098+JamesGrissom@users.noreply.github.com>
2022-03-14 17:49:44 -0400

    First Try At An FFT
    First Try At An FFT

--- EEG_Code/EEG_Code.ino
+++ EEG_Code/EEG_Code.ino
@@ -1,4 +1,3 @@
-#include "arduinoFFT.h"
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
@@ -9,27 +8,23 @@
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 
-arduinoFFT FFT = arduinoFFT();
 
-const uint16_t samples = 64;
-const double sampling = 40;
-const uint8_t amplitude = 4;
-uint8_t exponent;
-const double startFrequency = 2;
-const double stopFrequency = 16.4;
-const double step_size = 0.1;
+// This will allow us to use the byte data type instead of floats when calculating sin and cos in the FFT
 
-double vReal[samples];
-double vImag[samples];
+byte sine_data [91] = {
+  0,
+  4,    9,    13,   18,   22,   27,   31,   35,   40,   44,
+  49,   53,   57,   62,   66,   70,   75,   79,   83,   87,
+  91,   96,   100,  104,  108,  112,  116,  120,  124,  127,
+  131,  135,  139,  143,  146,  150,  153,  157,  160,  164,
+  167,  171,  174,  177,  180,  183,  186,  189,  192,  195,
+  198,  201,  204,  206,  209,  211,  214,  216,  219,  221,
+  223,  225,  227,  229,  231,  233,  235,  236,  238,  240,
+  241,  243,  244,  245,  246,  247,  248,  249,  250,  251,
+  252,  253,  253,  254,  254,  254,  255,  255,  255,  255
+};
 
-unsigned long time;
-
-
-#define SCL_INDEX 0x00
-#define SCL_TIME 0x01
-#define SCL_FREQUENCY 0x02
-#define SCL_PLOT 0x03
-
+float f_peaks[5]; // 5 dominant frequencies in descending order
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
@@ -41,31 +36,24 @@ void loop() {
   const int sampleRate = 128;
   int sample = 0;
 
-  double dataArray[sampleRate];
+  int dataArray[sampleRate];
 
   while (sample <= sampleRate) {
-
     double signalVoltage = analogRead(1) / 204.8;
-
     dataArray[sample] = signalVoltage;
-
-    sample++;
-
     delay(round(1024 / sampleRate));
-
+    sample++;
   }
 
   for (int i = 0; i < sampleRate; i++) {
-
     Serial.println(dataArray[i]);
   }
 
-  // Computing The FFT
-
-
-
-
-
+  // Calling The FFT Function
+  FFT(dataArray, 128, 128);
+  Serial.println(f_peaks[0]);
+  Serial.println(f_peaks[1]);
+  delay(99999);
 
   double signalVoltage = analogRead(1) / 204.8;
 
@@ -111,4 +99,170 @@ void loop() {
   display.display();
 
   sample = 0;
+}
+
+float FFT(int in[], int N, float Frequency)
+{
+  unsigned int data[13] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048};
+  int a, c1, f, o, x;
+  a = N;
+
+  for (int i = 0; i < 12; i++)          //calculating the levels
+  {
+    if (data[i] <= a) {
+      o = i;
+    }
+  }
+
+  int in_ps[data[o]] = {};              //input for sequencing
+  float out_r[data[o]] = {};            //real part of transform
+  float out_im[data[o]] = {};           //imaginory part of transform
+
+  x = 0;
+  for (int b = 0; b < o; b++)           // bit reversal
+  {
+    c1 = data[b];
+    f = data[o] / (c1 + c1);
+    for (int j = 0; j < c1; j++)
+    {
+      x = x + 1;
+      in_ps[x] = in_ps[j] + f;
+    }
+  }
+
+
+  for (int i = 0; i < data[o]; i++)     // update input array as per bit reverse order
+  {
+    if (in_ps[i] < a)
+    {
+      out_r[i] = in[in_ps[i]];
+    }
+    if (in_ps[i] > a)
+    {
+      out_r[i] = in[in_ps[i] - a];
+    }
+  }
+
+
+  int i10, i11, n1;
+  float e, c, s, tr, ti;
+
+  for (int i = 0; i < o; i++)          //fft
+  {
+    i10 = data[i];                     // overall values of sine/cosine  :
+    i11 = data[o] / data[i + 1];       // loop with similar sine cosine:
+    e = 360 / data[i + 1];
+    e = 0 - e;
+    n1 = 0;
+
+    for (int j = 0; j < i10; j++)
+    {
+      c = cosine(e * j);
+      s = sine(e * j);
+      n1 = j;
+
+      for (int k = 0; k < i11; k++)
+      {
+        tr = c * out_r[i10 + n1] - s * out_im[i10 + n1];
+        ti = s * out_r[i10 + n1] + c * out_im[i10 + n1];
+
+        out_r[n1 + i10] = out_r[n1] - tr;
+        out_r[n1] = out_r[n1] + tr;
+
+        out_im[n1 + i10] = out_im[n1] - ti;
+        out_im[n1] = out_im[n1] + ti;
+
+        n1 = n1 + i10 + i10;
+      }
+    }
+  }
+
+  // Here forward, out_r contains amplitude and our_in conntains frequency (Hz)
+  for (int i = 0; i < data[o - 1]; i++)                             // getting amplitude from compex number
+  {
+    out_r[i] = sqrt(out_r[i] * out_r[i] + out_im[i] * out_im[i]);   // to  increase the speed delete sqrt
+    out_im[i] = i * Frequency / N;
+  }
+
+  x = 0;                                                            // peak detection
+  for (int i = 1; i < data[o - 1] - 1; i++)
+  {
+    if (out_r[i] > out_r[i - 1] && out_r[i] > out_r[i + 1])
+    { in_ps[x] = i;                                                 // in_ps array used for storage of peak number
+      x = x + 1;
+    }
+  }
+
+
+  s = 0;
+  c = 0;
+  for (int i = 0; i < x; i++)                                       // re-arraange as per magnitude
+  {
+    for (int j = c; j < x; j++)
+    {
+      if (out_r[in_ps[i]] < out_r[in_ps[j]])
+      { s = in_ps[i];
+        in_ps[i] = in_ps[j];
+        in_ps[j] = s;
+      }
+    }
+    c = c + 1;
+  }
+
+
+  for (int i = 0; i < 5; i++)                                       // updating f_peak array (global variable)with descending order
+  {
+    f_peaks[i] = out_im[in_ps[i]];
+  }
+}
+
+
+float sine(int i)
+{
+  int j = i;
+  float out;
+  while (j < 0) {
+    j = j + 360;
+  }
+  while (j > 360) {
+    j = j - 360;
+  }
+  if (j > -1   && j < 91) {
+    out = sine_data[j];
+  }
+  else if (j > 90  && j < 181) {
+    out = sine_data[180 - j];
+  }
+  else if (j > 180 && j < 271) {
+    out = -sine_data[j - 180];
+  }
+  else if (j > 270 && j < 361) {
+    out = -sine_data[360 - j];
+  }
+  return (out / 255);
+}
+
+float cosine(int i)
+{
+  int j = i;
+  float out;
+  while (j < 0) {
+    j = j + 360;
+  }
+  while (j > 360) {
+    j = j - 360;
+  }
+  if (j > -1   && j < 91) {
+    out = sine_data[90 - j];
+  }
+  else if (j > 90  && j < 181) {
+    out = -sine_data[j - 90];
+  }
+  else if (j > 180 && j < 271) {
+    out = -sine_data[270 - j];
+  }
+  else if (j > 270 && j < 361) {
+    out = sine_data[j - 270];
+  }
+  return (out / 255);
 }
