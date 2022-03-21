bb6ae40068aac79cce8835514f8f757d763595e7
James Grissom <72823098+JamesGrissom@users.noreply.github.com>
2022-03-21 17:11:41 -0400

    Updated Code using the KickFFT library

--- EEG_signal-parsing_using_KickFFT/EEG_signal-parsing_using_KickFFT.ino
+++ EEG_signal-parsing_using_KickFFT/EEG_signal-parsing_using_KickFFT.ino
@@ -2,7 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "KickFFT.h"
+#include "KickFFT.h"  // https://github.com/LinnesLab/KickFFT/tree/master/examples/EXAMPLE01_Basic
 
 // On an arduino UNO:       A4(SDA), A5(SCL)
 
@@ -10,18 +10,42 @@
 #define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 
+float sampleRate = 128;
+uint16_t samples = 128;
+
+float deltaLower = 0.5;
+float deltaUpper = 4;
+
+float thetaLower = 4;
+float thetaUpper = 8;
+
+float alphaLower = 8;
+float alphaUpper = 12;
+
+float betaLower = 12;
+float betaUpper = 35;
+
+float gammaLower = 35;
+float gammaUpper = 45;
+
 void setup() {
+
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   Serial.begin(9600);
 }
 
 void loop() {
-  const int sampleRate = 128;
-  int dataArray[sampleRate];
+
+  int16_t dataArray[samples];
+  uint32_t deltaMag[samples];
+  uint32_t thetaMag[samples];
+  uint32_t alphaMag[samples];
+  uint32_t betaMag[samples];
+  uint32_t gammaMag[samples];
 
   // Reads and records 128 data points from analog pin 1 in 1 second
-  for (sample = 0, sample < sampleRate, sample++) {
+  for (int sample = 0; sample < sampleRate; sample++) {
     dataArray[sample] = analogRead(1);
     delay(round(1024 / sampleRate));
   }
@@ -31,18 +55,20 @@ void loop() {
     Serial.println(dataArray[i]);
   }
 
-  // Determining the frequencies using arduinoFFT
+  // Determining the frequencies and magnitudes using KickFFT
 
-  KickFFT<Type>::fft(float fs, float f1, float f2, uint16_t samples, const Type data[], uint32_t mag[])
+  KickFFT<int16_t>::fft(sampleRate, deltaLower, deltaUpper, samples, dataArray, deltaMag);
+  KickFFT<int16_t>::fft(sampleRate, thetaLower, thetaUpper, samples, dataArray, thetaMag);
+  KickFFT<int16_t>::fft(sampleRate, alphaLower, alphaUpper, samples, dataArray, alphaMag);
+  KickFFT<int16_t>::fft(sampleRate, betaLower, betaUpper, samples, dataArray, betaMag);
+  KickFFT<int16_t>::fft(sampleRate, gammaLower, gammaUpper, samples, dataArray, gammaMag);
 
-  double signalVoltage = analogRead(1) / 204.8;
-
-  // Stores the calculated power of each type of brain wave
-  double delta = signalVoltage;
-  double theta = signalVoltage;
-  double alpha = signalVoltage;
-  double beta = signalVoltage;
-  double gamma = signalVoltage;
+  // Stores the calculated average magnitude of each type of brain wave
+  float delta = average(deltaMag, samples);
+  float theta = average(thetaMag, samples);
+  float alpha = average(alphaMag, samples);
+  float beta = average(betaMag, samples);
+  float gamma = average(gammaMag, samples);
 
   // Stores the bar graph hight for each type of brain wave
   int deltaGraph = round(11.6 * delta);
@@ -66,6 +92,7 @@ void loop() {
   display.print("B");
   display.setCursor(112, 0);
   display.print("G");
+  display.display();
 
   // Displays the data in a bar graph
   display.fillRect(0, (64 - deltaGraph), 24, deltaGraph, WHITE);
@@ -73,8 +100,13 @@ void loop() {
   display.fillRect(52, (64 - alphaGraph), 24, alphaGraph, WHITE);
   display.fillRect(78, (64 - betaGraph), 24, betaGraph, WHITE);
   display.fillRect(104, (64 - gammaGraph), 24, gammaGraph, WHITE);
-
   display.display();
+}
 
-  sample = 0;
+float average (uint32_t *Array, int count) {
+  int sum = 0;
+  for (int i = 0; i < count; i++) {
+    sum += Array [i] ;
+  }
+  return ((float)sum / count);
 }
