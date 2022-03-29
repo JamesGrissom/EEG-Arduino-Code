57e69854784d6b8e53e59860f66bb32fb35bdc5c
James Grissom <72823098+JamesGrissom@users.noreply.github.com>
2022-03-28 20:34:48 -0400

    Milestone 3 update
    Currently runs only on Arduino Mega due to memory limitations of the Arduino Mega.
    
    Function Array References may need to be changed to function as intended.

--- EEG_Using_arduinoFFT.h/EEG_Using_arduinoFFT.h.ino
+++ EEG_Using_arduinoFFT.h/EEG_Using_arduinoFFT.h.ino
@@ -11,30 +11,37 @@ double vReal[samples];             // Each value is a frequency bin, stores the 
 double vImag[samples];
 arduinoFFT FFT = arduinoFFT(vReal, vImag, dataArray, sampleRate);
 
-// On an arduino UNO:       A4(SDA), A5(SCL)
-
+// On an Arduino:      A4(SDA), A5(SCL)
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(128, 64, &Wire, OLED_RESET);
 
-// Stores the calculated average magnitude of each type of brain wave
-double delta;
-double theta;
-double alpha;
-double beta;
-double gamma;
+// Stores the scaled brainwave power to be displayed in a bar graph
+int waveGraph;
 
-// Stores the bar graph hight for each type of brain wave
-int deltaGraph;
-int thetaGraph;
-int alphaGraph;
-int betaGraph;
-int gammaGraph;
+// Function Declarations
+void clearGraph();
+double averagePower(double realArray[], double imaginaryArray[], int start, int finish);
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   Serial.begin(9600);
+
+  // Diplays Column Labels for the bar graph
+  display.setTextSize(0);
+  display.setTextColor(WHITE);
+  display.setCursor(8, 0);
+  display.print("D");
+  display.setCursor(34, 0);
+  display.print("T");
+  display.setCursor(60, 0);
+  display.print("A");
+  display.setCursor(86, 0);
+  display.print("B");
+  display.setCursor(112, 0);
+  display.print("G");
+  display.display();
 }
 
 void loop() {
@@ -56,63 +63,53 @@ void loop() {
   FFT.Compute(FFT_FORWARD);
   FFT.ComplexToMagnitude();
 
-  // Analysis of the FFT
-  for (int i = 2; i < (samples / 2); i++) {
-    if (i <= 4) delta += vReal[i];
-    if ((i > 4) && (i <= 8)) theta += vReal[i];
-    if ((i > 8) && (i <= 12)) alpha += vReal[i];
-    if ((i > 12) && (i <= 35)) beta += vReal[i];
-    if ((i > 35) && (i <= 45)) gamma += vReal[i];
-  }
+  // Clears the graph so it can display the new set of data
+  clearGraph();
 
-  // Prints the brainwave values to to serial monitor for debugging purposes
-  Serial.print(delta);
-  Serial.print(" ");
-  Serial.print(theta);
-  Serial.print(" ");
-  Serial.print(alpha);
-  Serial.print(" ");
-  Serial.print(beta);
-  Serial.print(" ");
-  Serial.println(gamma);
+  // Analysis of the FFT, the numbers represent the brainwave frequency ranges
 
-  // calculates and stores the bar graph hight for each type of brain wave
-  deltaGraph = round(0.01 * delta);
-  thetaGraph = round(0.01 * theta);
-  alphaGraph = round(0.01 * alpha);
-  betaGraph = round(0.01 * beta);
-  gammaGraph = round(0.01 * gamma);
-
-  display.clearDisplay();
-
-  // Diplays Column Labels for the bar graph
-  display.setTextSize(0);
-  display.setTextColor(WHITE);
-  display.setCursor(8, 0);
-  display.print("D");
-  display.setCursor(34, 0);
-  display.print("T");
-  display.setCursor(60, 0);
-  display.print("A");
-  display.setCursor(86, 0);
-  display.print("B");
-  display.setCursor(112, 0);
-  display.print("G");
+  // Computes and displays the relative power of delta waves in the first bar of the bar graph
+  waveGraph = (int)averagePower(vReal, vImag, 1, 4);
+  display.fillRect(0, (64 - waveGraph), 24, waveGraph, WHITE);
   display.display();
+  waveGraph = 0;  // Resets waveaGraph value so the variable can be reused for the next wave type
 
-  // Displays the data in a bar graph
-  display.fillRect(0, (64 - deltaGraph), 24, deltaGraph, WHITE);
-  display.fillRect(26, (64 - thetaGraph), 24, thetaGraph, WHITE);
-  display.fillRect(52, (64 - alphaGraph), 24, alphaGraph, WHITE);
-  display.fillRect(78, (64 - betaGraph), 24, betaGraph, WHITE);
-  display.fillRect(104, (64 - gammaGraph), 24, gammaGraph, WHITE);
+  // Computes and displays the relative power of theta waves in the second bar of the bar graph
+  waveGraph = (int)averagePower(vReal, vImag, 4, 8);
+  display.fillRect(26, (64 - waveGraph), 24, waveGraph, WHITE);
   display.display();
+  waveGraph = 0;  // Resets waveaGraph value so the variable can be reused for the next wave type
+
+  // Computes and displays the relative power of alpha waves in the third bar of the bar graph
+  waveGraph = (int)averagePower(vReal, vImag, 8, 12);
+  display.fillRect(52, (64 - waveGraph), 24, waveGraph, WHITE);
+  display.display();
+  waveGraph = 0;  // Resets waveaGraph value so the variable can be reused for the next wave type
+
+  // Computes and displays the relative power of beta waves in the fourth bar of the bar graph
+  waveGraph = (int)averagePower(vReal, vImag, 12, 35);
+  display.fillRect(78, (64 - waveGraph), 24, waveGraph, WHITE);
+  display.display();
+  waveGraph = 0;  // Resets waveaGraph value so the variable can be reused for the next wave type
+
+  // Computes and displays the relative power of gamma waves in the fifth bar of the bar graph
+  waveGraph = (int)averagePower(vReal, vImag, 35, 45);
+  display.fillRect(104, (64 - waveGraph), 24, waveGraph, WHITE);
+  display.display();
+  waveGraph = 0;  // Resets waveaGraph value so the variable can be reused for the next wave type
 }
 
-float average (uint32_t *Array, int count) {
-  int sum = 0;
-  for (int i = 0; i < count; i++) {
-    sum += Array [i] ;
+void clearGraph() {
+  display.fillRect(0, 6, 128, 64, BLACK);
+  display.display();
+  return; 
+}
+
+double averagePower(double realArray[], double imaginaryArray[], int start, int finish) {
+  double sum;
+  int coefficientCount = finish - start;
+  for (start; start < finish; start++) {
+    sum = (sq(realArray[start]) + sq(imaginaryArray[start]));
   }
-  return ((float)sum / count);
+  return sqrt(sum / coefficientCount);
 }
